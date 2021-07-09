06086f61cdfd3062c3686413b90e15171d1d7a38
espisepi <sepinaco@gmail.com>
2021-07-09 20:15:42 +0200

    Reservation component helloworld

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
+import Reservation from "./components/reservation/Reservation";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Reservation />
     </div>
   );
 }
